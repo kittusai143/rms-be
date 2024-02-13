846228c77a7803607c2aed0abaed0c7b9fc7bf53
SAI CHARAN REDDY <79386814+204g1a0584@users.noreply.github.com>
2024-02-13 19:14:58 +0530

    BE working fine

--- src/main/java/com/sentrifugo/PerformanceManagementApplication.java
+++ src/main/java/com/sentrifugo/PerformanceManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class  PerformanceManagementApplication {
 	public static void main(String[] args) {
-
+		System.out.println("Application started");
 		SpringApplication.run(PerformanceManagementApplication.class,args);
 	}
 
