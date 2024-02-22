9885b8a33ce42939e54bb47863f26de7f01e2fa8
abhiramdanala <abhiramdanala@gmail.com>
2024-02-22 16:30:33 +0530

    modified api returns

--- src/main/java/com/sentrifugo/performanceManagement/repository/EmployeeRepo.java
+++ src/main/java/com/sentrifugo/performanceManagement/repository/EmployeeRepo.java
@@ -11,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 //FIELDS IN EMPLOYEE
 //id
 //user_id
@@ -46,7 +47,7 @@ public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
     List<Employee> findByProject(String project);
 
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findEmpDetails();
+    List<Map<String,Object>> findEmpDetails();
 
     @Query("SELECT DISTINCT e.client FROM Employee e")
     List<String> findDistinctByClient();
@@ -60,34 +61,34 @@ public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
     @Query("SELECT e.id FROM Users e  WHERE e.name IN :managerNames")
     List<Integer> findIdsByManagerIn(@Param("managerNames") List<String> managerNames);
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id and employee.client in :clients ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findByClientIn(List<String> clients);
+    List<Map<String,Object>> findByClientIn(List<String> clients);
 
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id and employee.project in :projects ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findByProjectIn(List<String> projects);
+    List<Map<String,Object>> findByProjectIn(List<String> projects);
 
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id and employee.reporting_manager in :managers ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findByReportingManagerIn(List<Integer> managers);
+    List<Map<String,Object>> findByReportingManagerIn(List<Integer> managers);
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id and employee.reporting_manager in :managers and client in :clients ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findByReportingManagerInAndClientIn( List<Integer> managers, List<String> clients);
+    List<Map<String,Object>> findByReportingManagerInAndClientIn( List<Integer> managers, List<String> clients);
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id  and employee.client in :clients and employee.project in :projects ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]>findByProjectInAndClientIn(List<String> projects,List<String> clients);
+    List<Map<String,Object>>findByProjectInAndClientIn(List<String> projects,List<String> clients);
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id and employee.reporting_manager in :managers  and employee.project in :projects ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findByProjectInAndReportingManagerIn(List<String> projects, List<Integer> managers);
+    List<Map<String,Object>> findByProjectInAndReportingManagerIn(List<String> projects, List<Integer> managers);
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id and employee.reporting_manager in :managers and employee.client in :clients and employee.project in :projects ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id",nativeQuery = true)
-    List<Object[]> findByProjectInAndReportingManagerInAndClientIn(List<String> projects, List<Integer> managers, List<String> clients);
+    List<Map<String,Object>> findByProjectInAndReportingManagerInAndClientIn(List<String> projects, List<Integer> managers, List<String> clients);
 
 
     //METHODS FOR THE TEAM OF A PARTICULAR MANAGER
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id where employee.reporting_manager=:manager ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id ",nativeQuery = true)
-    List<Object[]> findEmpDetailsByManager(int manager);
+    List<Map<String,Object>> findEmpDetailsByManager(int manager);
 
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id where employee.reporting_manager=:manager and employee.client in :clients and employee.project in :projects ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id ",nativeQuery = true)
-    List<Object[]> filterByClientAndProject(int manager,List<String> projects,List<String >clients);
+    List<Map<String,Object>> filterByClientAndProject(int manager,List<String> projects,List<String >clients);
 
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id where employee.reporting_manager=:manager and  employee.project in :projects ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id ",nativeQuery = true)
-    List<Object[]> filterByProject(int manager,List<String> projects);
+    List<Map<String,Object>> filterByProject(int manager,List<String> projects);
     @Query(value="select T.id,T.name,T.email,T.client,T.project,T.ReportingManager,Users.name as L2_Manager from(select E.*,Users.name as ReportingManager  from (select Users.id,Users.name,employee.reporting_manager,employee.l2_manager,Users.email,employee.client,employee.project   from Users join employee on Users.id=employee.Id where employee.reporting_manager=:manager and  employee.client in :clients ) as E join Users on Users.id=E.reporting_manager)as T join Users on T.l2_manager=Users.Id ",nativeQuery = true)
-    List<Object[]> filterByClient(int manager,List<String> clients);
+    List<Map<String,Object>> filterByClient(int manager,List<String> clients);
 
     @Query("SELECT DISTINCT e.department FROM Employee e")
     List<String> findDistinctByDepartment();
