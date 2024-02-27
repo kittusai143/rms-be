011d0eca6c18ffedccb2bb0a2785f0d6c3827552
BramhaT1 <112192465+BramhaT1@users.noreply.github.com>
2024-02-27 12:24:55 +0530

    h

--- src/main/java/com/sentrifugo/performanceManagement/vo/UserDetailsDto.java
+++ src/main/java/com/sentrifugo/performanceManagement/vo/UserDetailsDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @NoArgsConstructor
+@Data
 @AllArgsConstructor
 public class UserDetailsDto {
     private Integer id;
@@ -11,43 +12,5 @@ public class UserDetailsDto {
     private String employeeId;
     private String bussinessunit;
     private String department;
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getBussinessunit() {
-        return bussinessunit;
-    }
-
-    public void setBussinessunit(String bussinessunit) {
-        this.bussinessunit = bussinessunit;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 }
