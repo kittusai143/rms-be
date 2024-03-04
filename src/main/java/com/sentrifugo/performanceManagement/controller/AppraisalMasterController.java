6a329d8e672ff60a766a15a833a9a9802a97f398
deepika-reddy-b <149035324+deepika-reddy-b@users.noreply.github.com>
2024-03-04 15:37:28 +0530

    getting questions

--- src/main/java/com/sentrifugo/performanceManagement/controller/AppraisalMasterController.java
+++ src/main/java/com/sentrifugo/performanceManagement/controller/AppraisalMasterController.java
@@ -1,13 +1,24 @@
 package com.sentrifugo.performanceManagement.controller;
 
+import com.sentrifugo.performanceManagement.repository.AppraisalMasterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
-@CrossOrigin(origins = "${custom.frontendUrl}")
 @RequestMapping("/appraisal")
+@CrossOrigin(origins = "${custom.frontendUrl}")
 public class AppraisalMasterController {
 
+    @Autowired
+    private AppraisalMasterRepository appraisalMasterRepository;
+    @GetMapping("/getactivestatusEmp")
+    public List<Map<String,Object>> findActiveAppraisalStatus(){
+        List<Map<String,Object>> res= appraisalMasterRepository.findActiveStatusOfEmployee();
+        System.out.println(res);
+        return appraisalMasterRepository.findActiveStatusOfEmployee();
+    }
 }
--- src/main/java/com/sentrifugo/performanceManagement/repository/AppraisalMasterRepository.java
+++ src/main/java/com/sentrifugo/performanceManagement/repository/AppraisalMasterRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -30,6 +31,10 @@ public interface AppraisalMasterRepository extends JpaRepository<AppraisalMaster
 
     @Query("select Id from AppraisalMaster where employeeId=:id and isActive=true")
     Integer findAppraisalMasterId(Integer id);
+
+    @Query("SELECT a.id, a.employeeId, u.name, a.status, a.updatedDate FROM AppraisalMaster a JOIN Users u ON a.employeeId = u.id WHERE a.isActive = true")
+   List< Map<String,Object>> findActiveStatusOfEmployee();
+
 }
 
 //    @Query("SELECT am.Id FROM AppraisalMaster am WHERE am.employeeId = :id AND am.isActive = true")
